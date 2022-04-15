67dd1ffc243e671df90da19bebeecdecb1c7e885
poul <ezh1986@inbox.ru>
2022-04-15 09:01:53 +0700

    part 2 image,parent up data

--- src/components/e-catalog-item.vue
+++ src/components/e-catalog-item.vue
@@ -1,21 +1,38 @@
 <template>
-	<div class="e-catalog-item">
-		<p>item one</p>
-		<p>Price: 100</p>
-		<button>Pay</button>
-
-	</div>
+  <div class="e-catalog-item">
+    <img class="e-catalog-item__image" :src="require ('../assets/images/' + product_data.image)" alt="" />
+    <p class="e-catalog-item__name">{{product_data.name}}</p>
+    <p class="e-catalog-item__price">Price: {{product_data.price}} р</p>
+    <button 
+		class="e-catalog-item__btn btn" 
+		@click="sendDataToParent"
+		>Add to cart
+	</button>
+  </div>
 </template>
 
 <script>
 export default {
-	name: 'e-catalog-item',
-	props: {},
-	data (){
-		return{}
-	},
-	computed: {}
-}
+  name: "e-catalog-item",
+  props: {
+    product_data: {
+      type: Object,
+      default() {
+        return {};
+      }
+    }
+  },
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {
+	  sendDataToParent(){
+		  // передаем в родителя по клику данные
+		this.$emit( 'sendDataToParent', this.product_data.article)  
+	  }
+  }
+};
 </script>
 <style lang="scss">
 $padding: 8px;
@@ -23,11 +40,13 @@ $margin: 8px;
 
 $radius: 4px;
 
- .e-catalog-item{
-	 flex-basis: 25%;
-	 box-shadow: 0 0 8px 0 #e0e0e0;
-	 padding: $padding*2;
-	 margin-bottom: $margin*2;
- }
-
+.e-catalog-item {
+  flex-basis: 25%;
+  box-shadow: 0 0 8px 0 #e0e0e0;
+  padding: $padding * 2;
+  margin-bottom: $margin * 2;
+  	&__image{
+		  width: 100px;
+	  }
+}
 </style>
--- src/components/e-catalog.vue
+++ src/components/e-catalog.vue
@@ -1,37 +1,96 @@
 <template>
-	<div class="e-catalog">
-		<h1>Catalog</h1>
-		<div class="e-catalog__list">
-			<e-catalog-item />
-		</div>
-
-	</div>
+  <div class="e-catalog">
+    <h1>Catalog</h1>
+    <div class="e-catalog__list">
+      <e-catalog-item 
+	  	v-for="product in products"
+		:key="product.article"
+		v-bind:product_data="product"
+		@sendDataToParent="showChildArticleControle"
+	  />
+    </div>
+  </div>
 </template>
 
 <script>
-import eCatalogItem from './e-catalog-item.vue'
+import eCatalogItem from "./e-catalog-item.vue";
 
 export default {
-	name: 'e-catalog',
-	components:{
-		eCatalogItem
-	},
-	props:{},
-	data() {
-		return{}
-	},
-	computed: {}
-}
+  name: "e-catalog",
+  components: {
+    eCatalogItem,
+  },
+  props: {},
+  data() {
+    return {
+      products: [
+        {
+          image: "1.jpg",
+          name: "T-shirt 1",
+          price: 111,
+          article: "T1",
+          available: true,
+          category: "Мужские",
+        },
+        {
+          image: "2.jpg",
+          name: "T-shirt 2",
+          price: 222,
+          article: "T2",
+          available: true,
+          category: "Женские",
+        },
+        {
+          image: "3.jpg",
+          name: "T-shirt 3",
+          price: 333,
+          article: "T3",
+          available: false,
+          category: "Женские",
+        },
+        {
+          image: "4.jpg",
+          name: "T-shirt 4",
+          price: 444,
+          article: "T4",
+          available: true,
+          category: "Мужские",
+        },
+        {
+          image: "5.jpg",
+          name: "T-shirt 5",
+          price: 555,
+          article: "T5",
+          available: false,
+          category: "Женские",
+        },
+        {
+          image: "6.jpeg",
+          name: "T-shirt 6",
+          price: 666,
+          article: "T6",
+          available: true,
+          category: "Женские",
+        },
+      ],
+    };
+  },
+  computed: {},
+  methods:{
+	  showChildArticleControle(data){
+		  console.log(data);
+	  }
+
+  }
+};
 </script>
 <style lang="scss">
-.e-catalog{
-	&__list{
-		display: flex;
-		flex-wrap: wrap;
-		justify-content: space-between;
-		align-items: center;
-	}
-
+.e-catalog {
+  &__list {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    align-items: center;
+  }
 }
-
 </style>
